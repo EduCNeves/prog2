90ac4544acad93df7a0f21a122c0869416c817ea
Eduardo Camargo Neves <56937861+EduCNeves@users.noreply.github.com>
2022-06-30 17:02:09 -0300

    Update leitura.h

--- Sumarização de Dados GPS/leitura.h
+++ Sumarização de Dados GPS/leitura.h
@@ -7,8 +7,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+typedef struct Bicicleta_t {
+    
+    char nome;  
+
+}Bicicleta_t;*/
+
 
 void ler_arquivo (char* log);
 void ler_diretorio(char *diretorio);
      
 #endif
+
